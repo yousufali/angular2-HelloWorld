367a6647e6b6a112f74f69e4ccf30547ec178610
yousuf <mail.yousufali@gmail.com>
2016-09-18 13:27:11 -0500

    angular2 HelloWorld Checkin

---
+++ app.ts
@@ -0,0 +1,26 @@
+import { NgModule, Component
+} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+@Component({
+  selector: 'hello-world',
+  template: `
+  <ul>
+    <li *ngFor="let name of names">Hello {{ name }}</li>
+</ul>
+`
+})
+class HelloWorld {
+    names: string[];
+    constructor(){   
+        this.names = ["Yousuf", "Ayaan", "Afreen"];
+    }
+}
+
+@NgModule({
+  declarations: [ HelloWorld ],
+  imports: [ BrowserModule ],
+  bootstrap: [ HelloWorld ],
+})
+class HelloWorldAppModule {}
+platformBrowserDynamic().bootstrapModule(HelloWorldAppModule);
